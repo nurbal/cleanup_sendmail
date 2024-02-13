f7caaebd231eaf80a9f881f8d028947842dc49ec
Bruno Carrez <bruno.carrez@mila.quebec>
2024-02-12 23:32:57 -0500

    switch send method to gmail smtp, with app password

--- README.md
+++ README.md
@@ -12,3 +12,17 @@ The name should be in the form `client_secret_<clientID>.json` where clientID is
 The program will just scan the folder to retrieve these informations from the first json file found in `secrets/gmail/`.
 
 You also need a `token.json` file. If no token is available, you can run `python gmail.py <your mail address>` to generate this token file and send you a test mail.
+
+## Gmail account & app password
+
+You will need to create an "app password" for the used gmail account.
+To create it, go to google account settings->security->2-step-verification->app password (bottom of the page)
+
+Then put the account details in secrets/gmail/account.json :
+```
+{
+    "sender": "mailbot.accountname@mila.quebec",
+    "password": "lorem ipsum"
+}
+
+```
--- gmail.py
+++ gmail.py
@@ -1,90 +1,35 @@
 import argparse
 import json
 import os
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
-import google.auth
-from email.message import EmailMessage
-import base64
+
+import smtplib
+from email.mime.text import MIMEText
+
 
 mydir = os.path.dirname(os.path.realpath(__file__))+'/'
 
-def getSecretFile():
-    directory = mydir+'secrets/gmail/'
-    files = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
-    for file in files:
-        if file.endswith('.json') and file.startswith('client_secret_'):
-            return directory+file
-    return None
-
-
 def getCrendentials():
-    creds = None
-    token_path = mydir+"secrets/gmail/token.json"
-    SCOPES = ["https://www.googleapis.com/auth/gmail.compose"]
-
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists(token_path):
-        creds = Credentials.from_authorized_user_file(token_path, SCOPES)
+    # load the json file containing the credentials
+    account_file = mydir+'secrets/gmail/account.json'
+    if os.path.exists(account_file):
+        with open(account_file, "r") as f:
+            creds = json.load(f)
+            print(f'Credentials found in {account_file}')
     if not creds:
         print('No credentials found in secrets/gmail/')
-    if not creds.valid:
-        print('Invalid credentials found in secrets/gmail/')
-
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        # scan secrets/gmail/ to retrieve secret key
-        secret_file = getSecretFile()
-        if secret_file is None:
-            print('No clientID found in secrets/gmail/')
-            exit(1) 
-
-        flow = InstalledAppFlow.from_client_secrets_file(secret_file, SCOPES)
-        creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open(token_path, "w") as token:
-            token.write(creds.to_json())
-    else:
-        print(f'Credentials found in {token_path}')
-
     return creds
 
 def sendMessage(creds,target_email, subject, body):
+    # send mail
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = creds['sender']
+    msg['To'] = target_email
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
+       smtp_server.login(creds['sender'], creds['password'])
+       smtp_server.sendmail(creds['sender'], target_email, msg.as_string())
+    print("Message sent!")
 
-    try:
-        # create gmail api client
-        service = build("gmail", "v1", credentials=creds)
-
-        message = EmailMessage()
-
-        message.set_content(body)
-
-        message["To"] = target_email
-        message["From"] = "noreply@mila.quebec"
-        message["Subject"] = subject
-
-        # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
-
-        create_message = {"raw": encoded_message}
-        # pylint: disable=E1101
-        send_message = (
-            service.users()
-            .messages()
-            .send(userId="me", body=create_message)
-            .execute()
-        )
-
-    except HttpError as error:
-        print(f"An error occurred: {error}")
-        send_message = None
-
-    return send_message
 
 
 
